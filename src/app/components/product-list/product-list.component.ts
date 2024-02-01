180843c944881191775cb7d9fe3e02083abf762f
Mitesh Vaghela <mitesh.vaghela@openxcell.com>
2024-02-01 17:11:16 +0530

    Fix bugs

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NgxMasonryOptions } from 'ngx-masonry';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-
+  public myOptions: NgxMasonryOptions = {
+    gutter: 10,
+    columnWidth: '.grid-sizer',
+    itemSelector: '.grid-item',
+    // columnWidth: 200,
+    // itemSelector: '.mat-card'
+  };
 }
