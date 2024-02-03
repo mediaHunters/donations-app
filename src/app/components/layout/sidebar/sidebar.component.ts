af141d030089719a14cc0ec9098754a4d3733776
Mitesh Vaghela <miteshvaghela1999@gmail.com>
2024-02-03 10:17:39 +0530

    Sidebar Done

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { SidebarService } from '../services/sidebar.service';
 import { iconAnimation, labelAnimation, sidebarAnimation } from '../../../animations';
-
+import { MediaMatcher } from '@angular/cdk/layout';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -16,9 +16,17 @@ import { iconAnimation, labelAnimation, sidebarAnimation } from '../../../animat
 export class SidebarComponent implements OnInit {
   sidebarState!: string;
   panelOpenState: boolean = false;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
   constructor(
     private sidebarService: SidebarService,
-  ) { }
+    media: MediaMatcher,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { 
+    this.mobileQuery = media.matchMedia('(max-width: 600px)'); // Adjust the breakpoint as needed
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
     this.sidebarService.sidebarStateObservable$.
