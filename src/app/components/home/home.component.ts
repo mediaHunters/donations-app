3db9c1bcdef1bd952764722c7819a7786d28e3ac
Mitesh Vaghela <miteshvaghela1999@gmail.com>
2024-02-18 14:14:41 +0530

    Fix bugs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+  }
 
+  routeNavigate(url: string) {
+    this.router.navigate([url]);
+  }
 }
